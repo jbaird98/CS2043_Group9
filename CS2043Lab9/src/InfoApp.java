d36df57af8889d03b7a6db2e87ede0d20dc1d549
yTJAMxBnZazcPhtURpbgwLErYdCu-DqsHmKWvQV  Senoir <33429210+yTJAMxBnZazcPhtURpbgwLErYdCu-DqsHmKWvQV@users.noreply.github.com>
2017-12-06 19:21:49 -0400

    Update InfoApp.java

--- CS2043Lab9/src/InfoApp.java
+++ CS2043Lab9/src/InfoApp.java
@@ -1,6 +1,11 @@
 
 public class InfoApp {
 	public static void main(String[] args) {
+		InfoPrintJacob jacob = new InfoPrintJacob();
+		InfoPrintBeasley beasley = new InfoPrintBeasley();
 		System.out.println("Team 9 member list:");
+		jacob.InfoPrintJacob();
+		beasley.InfoPrintBeasley();
+		
 	}
 }
